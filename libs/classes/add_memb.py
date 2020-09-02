5c8f51677ba5b2a67f7746341a4d8d0b64b6eeff
Bernardin <deziusb@knights.ucf.edu>
2020-09-01 22:46:17 -0400

    Now able to capture QRcode

--- libs/classes/add_memb.py
+++ libs/classes/add_memb.py
@@ -1,43 +1,58 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen
 
+import cv2
+import numpy as np
+
 class AddMember(Screen):
    
-	def __init__(self, **kwargs):
-		super(AddMember, self).__init__(**kwargs)
-		self.app = MDApp.get_running_app()
+    def __init__(self, **kwargs):
+        super(AddMember, self).__init__(**kwargs)
+        self.app = MDApp.get_running_app()
 
-	def on_pre_enter(self):
-		self.ids['intent_button'].disabled = False
-		self.ids['first_name'].text = ''
-		self.ids['last_name'].text = ''
-		self.ids['mem_email'].text = ''
-		self.ids['mem_phone'].text = ''
-		self.ids['confirm_button'].disabled = True
-		self.ids['again_button'].disabled = True
-		self.ids['home_button'].disabled = True
-		
-	
-	def add_member(self, button, first_name, last_name, email, phone):
-		'''
-		Adds new member to spreadsheet
-		'''
-		
-		# Add to spreadsheet
-		creds = self.app.get_creds()
-		if creds.access_token_expired:
-			try:
-				self.app.sheet = get_spread()
-			except:
-				self.app.spread_unloaded()
-		self.app.sheet.append_row([first_name.strip(),last_name.strip(), 'Beginner', 0, 'No', email, phone])
-		button.disabled = True
-		self.ids['home_button'].disabled = False
-		self.ids['again_button'].disabled = False
-		
-		# Create QR Code
-		self.ids['qr'].data = "{},{},{}".format(self.app.club_striped,first_name,last_name)
+    def on_pre_enter(self):
+        self.ids['intent_button'].disabled = False
+        self.ids['first_name'].text = ''
+        self.ids['last_name'].text = ''
+        self.ids['mem_email'].text = ''
+        self.ids['mem_phone'].text = ''
+        self.ids['confirm_button'].disabled = True
+        self.ids['again_button'].disabled = True
+        self.ids['home_button'].disabled = True
+        
+    
+    def add_member(self, button, first_name, last_name, email, phone):
+        '''
+        Adds new member to spreadsheet
+        '''
+        
+        '''self.export_to_png, self.export_as_image'''
+        
+        # Add to spreadsheet
+        creds = self.app.get_creds()
+        if creds.access_token_expired:
+            try:
+                self.app.sheet = get_spread()
+            except:
+                self.app.spread_unloaded()
+        self.app.sheet.append_row([first_name.strip(),last_name.strip(), 'Beginner', 0, 'No', email, phone])
+        button.disabled = True
+        self.ids['home_button'].disabled = False
+        self.ids['again_button'].disabled = False
+        
+        # Create QR Code
+        self.ids['qr'].data = "{},{},{}".format(self.app.club_striped,first_name,last_name)
+        
+        # Get and crop out QRcode from screenshot of current page
+        texture = self.ids.qrBox.export_as_image("test").texture
+        new_img = np.frombuffer(texture.pixels, np.uint8)
+        height, width = texture.height, texture.width
+        img = new_img.reshape(height, width, 4)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        cv2.imshow("img", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-	def confirm(self, button):
-		button.disabled = True
-		self.ids['confirm_button'].disabled = False
+    def confirm(self, button):
+        button.disabled = True
+        self.ids['confirm_button'].disabled = False
